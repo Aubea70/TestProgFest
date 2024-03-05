e8525e7e0e5ab2208d24d0934aef8d524d8c3810
Aurélie Beaudoin <aurelie.beaudoin.1@ulaval.ca>
2024-03-05 10:31:20 -0500

    ajouté infos sur deleting branches

--- testprogfest.py
+++ testprogfest.py
@@ -4,7 +4,8 @@ print('Hello World!!!')
 To create a new branch: git branch {name of branch}
 To go to branch: git checkout {name of branch}
 To do both at the same time: git checkout b- {name of branch}
-To merge branges: go to the branch you want to merge into, git merge {neme of branch you want to merge}
+To merge branches: go to the branch you want to merge into, git merge {neme of branch you want to merge}
+To delete a branch: git  ranch -d {name of branch}
 '''
 
 print('Ceci est un test')
