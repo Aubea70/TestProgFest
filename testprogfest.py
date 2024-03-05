7a51f13207c9c6d941234192d133af0a31712dc1
Aurélie Beaudoin <aurelie.beaudoin.1@ulaval.ca>
2024-03-05 10:29:57 -0500

    description sur les branches

--- testprogfest.py
+++ testprogfest.py
@@ -1,5 +1,10 @@
 print('Hello World!!!')
 
-'''To create a new branch: git checkout -b {name of branch}'''
+'''
+To create a new branch: git branch {name of branch}
+To go to branch: git checkout {name of branch}
+To do both at the same time: git checkout b- {name of branch}
+To merge branges: go to the branch you want to merge into, git merge {neme of branch you want to merge}
+'''
 
 print('Ceci est un test')
